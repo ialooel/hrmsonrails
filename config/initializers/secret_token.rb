a3b0a5971019f8a64e4aa854c57585e6c9427a5c
ialooel <ialooel@ubuntu12.(none)>
2012-06-23 23:57:58 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrmsonrails::Application.config.secret_token = 'a21e3a8e5a2acfeb79ca62c7e0111328c57e304bac970c4091357516639dd879939a1e77f5d87f1caed762d4843438fa21bf8b632b45f0b2317f15e32779c8c7'
